3506b2ac3bd8f41277bf72e088d57f5426cfe6d3
Finn Neron <72130813+DennisRono@users.noreply.github.com>
2021-03-27 14:44:04 +0300

    Add files via upload
    login commit

---
+++ java-atm-system/src/dennisrono/atm.java
@@ -0,0 +1,14 @@
+package dennisrono;
+public class atm {
+
+	public static void main(String[] args) {
+		System.out.println("\tWelcome to our ATM\n");
+		System.out.println("\n");
+		login i = new login();
+		i.log();
+		System.out.println("\tWelcome ");
+		System.out.println("\tYour current balance is ");
+
+	}
+
+}
